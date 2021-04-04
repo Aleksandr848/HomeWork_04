a62b3eb6644d092387470a5cc1094560005eca3c
Aleksandr Maksimov <sm-1@inbox.lv>
2021-04-04 19:15:50 +0300

    Initial commit

---
+++ media_types.sql
@@ -0,0 +1,21 @@
+USE vk2;
+
+UPDATE media_types
+SET
+    name = 'text'
+WHERE id = 1;
+
+UPDATE media_types
+SET
+    name = 'photo'
+WHERE id = 2;
+
+UPDATE media_types
+SET
+    name = 'video'
+WHERE id = 3;
+
+UPDATE media_types
+SET
+    name = 'audio'
+WHERE id = 4;
